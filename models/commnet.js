251d7f017f6be59317393c6a6ec053cbfdd6478c
LaVi <lastvirgo266@gmail.com>
2020-03-02 17:08:24 +0900

    ADD : comment revise function

--- models/commnet.js
+++ models/commnet.js
@@ -1,10 +1,6 @@
 module.exports = (sequelize, DataTypes)=>(
 
     sequelize.define('commnet', {
-        title : {
-            type : DataTypes.STRING(40),
-            allowNull : false,
-        },
 
         contents : {
             type : DataTypes.STRING(200),
--- routes/todos.js
+++ routes/todos.js
@@ -4,6 +4,7 @@ const {Todo, Comment} = require('../models');
 const router = express.Router();
 
 
+/* Todos API  */
 router.post('/', (req,res,next)=>{
 
     console.log(req.body);
@@ -100,5 +101,91 @@ router.delete('/:id', (req,res,next)=>{
 });
 
 
+/* Comment API  */
+
+//Create comment
+router.post('/:id/comments', (req,res,next)=>{
+
+    console.log(req.body);
+    const { contents } = req.body;
+        Comment.create({
+            contents,
+            todoId : req.params.id
+        })
+        .then(result =>{
+            return res.status(200).json(result)
+        })
+        .catch(err =>{
+            console.error(err);
+            return res.status(500).json({
+                msg : "해당 게시물이 존재하지 않습니다."
+            });
+        })
+
+});
+
+
+
+
+//Read all comments
+router.get('/:id/comments', (req,res,next)=>{
+    Comment.findAll({where : {todoId : req.params.id}})
+    .then((result) =>{
+        return res.status(200).json(result);
+    })
+    .catch(err=>{
+        console.error(err);
+        return res.status(500);
+    })
+});
+
+
+//Read Specify comments
+router.get('/:todoId/comments/:commentId', (req,res,next)=>{
+
+    Comment.findAll({where : {todoId : req.params.todoId}})
+    .then((result) =>{
+        return res.status(200).json(result[req.params.commentId-1]);
+    })
+    .catch(err=>{
+        console.error(err);
+        return res.status(500).json({
+            msg : "존재하지 않습니다."
+        })
+    })
+});
+
+
+//Revise comment
+router.put('/:todoId/comments/:commentId', (req,res,next)=>{
+    const {contents} = req.body;
+
+    Comment.findAll({where : {todoId : req.params.todoId}})
+    .then((results) =>{
+        Comment.update({
+            contents
+        },
+        {where : {id : results[req.params.commentId-1].id}}
+        )
+        .then(()=>{
+            return Comment.findByPk(results[req.params.commentId-1].id);
+        })
+        .then(result=>{
+            return res.status(200).json(result);
+        })
+        .error(err =>{
+            console.error(err);
+            return res.status(500).json({
+                msg : "오류!"
+            })
+        })
+    })
+    .catch(err=>{
+        console.error(err);
+        return res.status(500).json({
+            msg : "존재하지 않습니다."
+        })
+    })
+});
 
 module.exports = router;
