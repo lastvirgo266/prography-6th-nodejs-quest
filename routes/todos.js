089dd821c658fbe04a832c6db07431e434043a29
LaVi <lastvirgo266@gmail.com>
2020-03-04 17:14:03 +0900

    Add : serach title, descirption function

--- routes/todos.js
+++ routes/todos.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const {Todo, Comment} = require('../models');
-const qs = require('qs');
-
+const sequelize = require('sequelize');
+const op = sequelize.Op;
 const router = express.Router();
 
 
@@ -31,9 +31,83 @@ router.post('/', (req,res,next)=>{
 
 //Get all todos(Add use the order query)
 router.get('/', (req,res,next)=>{
+
+    //order function
     if(req.query.order != undefined){
         console.log(req.query.order);
-        var orderKey= Object.keys(req.query.order)[0];
+        let orderKey= Object.keys(req.query.order)[0];
+        var orderValue = Object.values(req.query.order)[0];
+
+        Todo.findAll({
+            order : [[orderKey, orderValue]]
+        })
+        .then((result) =>{
+            
+            for(var i=0; i< result.length; i++)
+                result[i].tags = JSON.parse(result[i].tags);
+
+            return res.status(200).json(result);
+        })
+        .catch(err=>{
+            console.error(err);
+            return res.status(500);
+        })
+    }
+
+    //Search function
+    else if(req.query.title != undefined){
+        console.log(req.query.title);
+        let keyword= req.query.title;
+
+        Todo.findAll({
+           where : {
+               title :{ 
+                [op.like] : keyword 
+            }
+          }
+        })
+        .then((result) =>{
+            
+            for(var i=0; i< result.length; i++)
+                result[i].tags = JSON.parse(result[i].tags);
+
+            return res.status(200).json(result);
+        })
+        .catch(err=>{
+            console.error(err);
+            return res.status(500);
+        })
+    }
+
+    //serach description
+    else if(req.query.description != undefined){
+        console.log(req.query.description);
+        let keyword= req.query.description;
+
+        Todo.findAll({
+           where : {
+               description :{ 
+                [op.like] : keyword 
+            }
+          }
+        })
+        .then((result) =>{
+            
+            for(var i=0; i< result.length; i++)
+                result[i].tags = JSON.parse(result[i].tags);
+
+            return res.status(200).json(result);
+        })
+        .catch(err=>{
+            console.error(err);
+            return res.status(500);
+        })
+    }
+
+    //search tags
+    else if(req.query.tags != undefined){
+        console.log(req.query.order);
+        let orderKey= Object.keys(req.query.order)[0];
         var orderValue = Object.values(req.query.order)[0];
 
         Todo.findAll({
